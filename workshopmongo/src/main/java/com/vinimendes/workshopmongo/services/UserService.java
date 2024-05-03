70036e3410512c9b61164a030d2dc684ae6a1bff
Vinicius Mendes <viniciusmendes2802@gmail.com>
2024-05-03 11:39:45 -0300

    Obtendo um usuario por id

--- workshopmongo/src/main/java/com/vinimendes/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/vinimendes/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.vinimendes.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vinimendes.workshopmongo.domain.User;
 import com.vinimendes.workshopmongo.repository.UserRepository;
+import com.vinimendes.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
